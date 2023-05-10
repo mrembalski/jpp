8e526d6a4e7f149f95c49892c3a146746a6faee4
mrembalski <31540274+mrembalski@users.noreply.github.com>
2023-05-10 22:27:16 +0200

    Print to stderr

--- src/Main.hs
+++ src/Main.hs
@@ -2,17 +2,18 @@ import Interpreter (evalMain, interpret)
 import Ruskell.Par (myLexer, pProgram)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
+import System.IO
 
 runFile :: FilePath -> IO ()
 runFile f = readFile f >>= runProgram
 
 runProgram :: String -> IO ()
 runProgram code = case pProgram $ myLexer code of
-  Left e -> putStrLn e >> exitFailure
+  Left e -> hPutStrLn stderr e >> exitFailure
   Right parsed -> case interpret parsed of
-    Left e -> putStrLn e >> exitFailure
+    Left e -> hPutStrLn stderr e >> exitFailure
     Right interpreted -> case evalMain interpreted of
-      Left e -> putStrLn e >> exitFailure
+      Left e -> hPutStrLn stderr e >> exitFailure
       Right val -> print val
 
 main :: IO ()
